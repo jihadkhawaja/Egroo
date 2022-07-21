507becba20c9c71b4558dd5887ff717951e065db
Jihad Khawaja <khawajajihad@gmail.com>
2022-07-22 00:34:12 +0300

    Update ServerChat.cs

--- src/JihadKhawaja.SignalR.Server.Chat/ServerChat.cs
+++ src/JihadKhawaja.SignalR.Server.Chat/ServerChat.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace JihadKhawaja.SignalR.Server.Chat
+﻿namespace JihadKhawaja.SignalR.Server.Chat
 {
     public static class ServerChat
     {
