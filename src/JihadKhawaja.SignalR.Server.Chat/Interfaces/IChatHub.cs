07d2a30d18bf88b09a5cee0466ffb5fd85a71d3c
Jihad Khawaja <khawajajihad@gmail.com>
2022-07-22 14:53:27 +0300

    sign in, sign up view

---
+++ src/JihadKhawaja.SignalR.Server.Chat/Interfaces/IChatHub.cs
@@ -0,0 +1,23 @@
+﻿using JihadKhawaja.SignalR.Server.Chat.Models;
+
+namespace JihadKhawaja.SignalR.Server.Chat.Interfaces
+{
+    public interface IChatHub
+    {
+        Task<KeyValuePair<Guid, bool>> SignUp(string displayname, string username, string email, string password);
+        Task<KeyValuePair<Guid, bool>> SignIn(string emailorusername, string password);
+        Task<bool> ChangePassword(string emailorusername, string newpassword);
+        Task<string> GetUserDisplayName(Guid userId);
+        Task<bool> AddFriend(Guid userId, string friendEmailorusername);
+        Task<bool> RemoveFriend(Guid userId, string friendEmailorusername);
+
+        Task<Channel> CreateChannel(Guid userId, params string[] usernames);
+        Task<User[]> GetChannelUsers(Guid channelid);
+        Task<Channel[]> GetUserChannels(Guid userid);
+
+        Task<bool> SendMessage(Message message);
+        Task<bool> UpdateMessage(Message message);
+        Task<Message[]> ReceiveMessageHistory(Guid channelId);
+        Task<Message[]> ReceiveMessageHistoryRange(Guid channelId, int index, int range);
+    }
+}
