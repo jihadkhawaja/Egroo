f9c28bea199320d6103bca6d0280b3a3ecd7b9ab
Jihad Khawaja <khawajajihad@gmail.com>
2022-11-30 10:50:52 +0200

    #48 fix

--- src/MobileChat.Server/Program.cs
+++ src/MobileChat.Server/Program.cs
@@ -2,6 +2,8 @@ using Serilog;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
+Configurations = builder.Configuration;
+
 //logger
 builder.Host.UseSerilog((ctx, lc) => lc.ReadFrom.Configuration(ctx.Configuration));
 
@@ -15,7 +17,12 @@ builder.Services.AddCors(options =>
 });
 
 //mobile chat service
-builder.Services.AddMobileChatServices(builder.Configuration, ServiceCollectionEx.DatabaseEnum.Postgres, typeof(Program));
+builder.Services.AddMobileChatServices(
+    builder.Configuration, 
+    typeof(Program), 
+    ServiceCollectionEx.DatabaseEnum.Postgres,
+    false, false
+    );
 
 WebApplication app = builder.Build();
 
@@ -34,7 +41,11 @@ app.UseRouting();
 app.UseCors("CorsPolicy");
 
 //chat hub
-//true to use JWT auth
-app.UseMobileChatServices(false);
+app.UseMobileChatServices();
 
 app.Run();
+
+public partial class Program
+{
+    public static ConfigurationManager Configurations { get; set; }
+}
