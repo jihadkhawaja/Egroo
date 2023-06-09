dea5bab716dcc40c1b90dd77b2a508a848a5c7ea
Jihad Khawaja <khawajajihad@gmail.com>
2023-06-09 15:07:49 +0300

    Implemented a shared UI

--- src/MobileChat.MAUI/MauiProgram.cs
+++ src/MobileChat.MAUI/MauiProgram.cs
@@ -1,27 +1,15 @@
-﻿using jihadkhawaja.mobilechat.client.Interfaces;
-using jihadkhawaja.mobilechat.client.Models;
-using jihadkhawaja.mobilechat.client.Services;
-using MobileChat.MAUI.Interfaces;
-using MobileChat.MAUI.Services;
-using jihadkhawaja.mobilechat.client;
-using MudBlazor.Services;
+﻿using jihadkhawaja.mobilechat.client.Models;
 using jihadkhawaja.mobilechat.client.Services.Extension;
+using MobileChat.UI.Constants;
+using MobileChat.UI.Core;
+using MobileChat.UI.Interfaces;
+using MobileChat.UI.Services;
+using MudBlazor.Services;
 
 namespace MobileChat.MAUI
 {
     public static class MauiProgram
     {
-        // SignalR chat hub name (http://your-web-url/hubName)
-        private const string HubName = "chathub";
-#if DEBUG
-        // Development
-        // SignalR Web URL example (http://localhost:2736/ or server IP address with port) where the chat web app is hosted
-        public const string HubConnectionURL = "http://localhost:5175/" + HubName;
-#else
-        // Production
-        // SignalR Web URL example (https://www.domain.com/ or server IP address with port) where the chat web app is hosted
-        public const string HubConnectionURL = "your address here" + HubName;
-#endif
         public static CancellationTokenSource ConnectionCancellationTokenSource { get; private set; }
         public static MauiApp CreateMauiApp()
         {
@@ -69,32 +57,7 @@ namespace MobileChat.MAUI
                 }
                 catch { }
 
-                Initialize(HubConnectionURL, SessionStorage.User?.Token);
-            }
-        }
-
-        public static void Initialize(string HubConnectionURL, string Token = "")
-        {
-            // Initialize client chat signalr service with your server chat hub url
-            jihadkhawaja.mobilechat.client.MobileChat.Initialize(HubConnectionURL, Token);
-        }
-
-        public static async Task Connect()
-        {
-            //connect to the server through SignalR chathub
-            ConnectionCancellationTokenSource = new();
-            if (await jihadkhawaja.mobilechat.client.MobileChat.SignalR.Connect(ConnectionCancellationTokenSource))
-            {
-                //client connected
-            }
-        }
-
-        public static async Task Disconnect()
-        {
-            //disconnect from the server through SignalR chathub
-            if (await jihadkhawaja.mobilechat.client.MobileChat.SignalR.Disconnect())
-            {
-                //client disconnected
+                HubInitializer.Initialize(Source.HubConnectionURL, SessionStorage.User?.Token);
             }
         }
     }
--- src/MobileChat.WASM/Program.cs
+++ src/MobileChat.WASM/Program.cs
@@ -2,8 +2,10 @@ using jihadkhawaja.mobilechat.client.Models;
 using jihadkhawaja.mobilechat.client.Services.Extension;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MobileChat.UI.Constants;
+using MobileChat.UI.Core;
+using MobileChat.UI.Services;
 using MobileChat.WASM;
-using MobileChat.WASM.Services;
 using MudBlazor.Services;
 using System.Text.Json;
 
@@ -12,19 +14,12 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-#if DEBUG
+
 builder.Services.AddScoped(sp => new HttpClient
 {
-    BaseAddress = new Uri("http://localhost:5175"),
+    BaseAddress = new Uri(Source.ConnectionURL),
     Timeout = TimeSpan.FromMinutes(10)
 });
-#else
-builder.Services.AddScoped(sp => new HttpClient
-{
-    BaseAddress = new Uri("https://www.your-domain.com/"),
-    Timeout = TimeSpan.FromMinutes(10)
-});
-#endif
 
 builder.Services.AddMudServices();
 
@@ -48,48 +43,7 @@ using (IServiceScope scope = app.Services.CreateScope())
     }
     catch { }
 
-    Initialize(HubConnectionURL, SessionStorage.User?.Token);
+    HubInitializer.Initialize(Source.HubConnectionURL, SessionStorage.User?.Token);
 }
 
 await app.RunAsync();
-
-public partial class Program
-{
-    // SignalR chat hub name (http://your-web-url/hubName)
-    private const string HubName = "chathub";
-#if DEBUG
-    // Development
-    // SignalR Web URL example (http://localhost:2736/ or server IP address with port) where the chat web app is hosted
-    public const string HubConnectionURL = "http://localhost:5175/" + HubName;
-#else
-        // Production
-        // SignalR Web URL example (https://www.domain.com/ or server IP address with port) where the chat web app is hosted
-        public const string HubConnectionURL = "https://www.your-domain.com/" + HubName;
-#endif
-    public static CancellationTokenSource ConnectionCancellationTokenSource { get; private set; }
-
-    public static void Initialize(string HubConnectionURL, string Token = "")
-    {
-        // Initialize client chat signalr service with your server chat hub url
-        jihadkhawaja.mobilechat.client.MobileChat.Initialize(HubConnectionURL, Token);
-    }
-
-    public static async Task Connect()
-    {
-        //connect to the server through SignalR chathub
-        ConnectionCancellationTokenSource = new();
-        if (await jihadkhawaja.mobilechat.client.MobileChat.SignalR.Connect(ConnectionCancellationTokenSource))
-        {
-            //client connected
-        }
-    }
-
-    public static async Task Disconnect()
-    {
-        //disconnect from the server through SignalR chathub
-        if (await jihadkhawaja.mobilechat.client.MobileChat.SignalR.Disconnect())
-        {
-            //client disconnected
-        }
-    }
-}
