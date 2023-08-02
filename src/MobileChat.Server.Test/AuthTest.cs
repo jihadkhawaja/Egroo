c385f76d5e6d726a83ae7fbc6050ae65ccbe2810
Jihad Khawaja <khawajajihad@gmail.com>
2023-08-02 12:32:34 +0300

    Added Tests + Cleanup

--- src/MobileChat.Server.Test/AuthTest.cs
+++ src/MobileChat.Server.Test/AuthTest.cs
@@ -1,29 +1,77 @@
+using jihadkhawaja.mobilechat.client.Interfaces;
+using jihadkhawaja.mobilechat.client.Services;
+using System.Text.Json;
+
 namespace MobileChat.Server.Test
 {
     [TestClass]
     public class AuthTest
     {
-        public async Task Connect()
+        private IChatAuth ChatAuthService { get; set; } = null!;
+
+        [TestInitialize]
+        public async Task Initialize()
         {
-            // Arrange
+            ChatAuthService = new ChatAuthService();
+
             var cancellationTokenSource = new CancellationTokenSource();
-            var hubConnectionUrl = "";
-
-            // Act
-            jihadkhawaja.mobilechat.client.MobileChat.Initialize(hubConnectionUrl);
-            var connected = await jihadkhawaja.mobilechat.client.MobileChat.SignalR.Connect(cancellationTokenSource);
-
-            // Assert
-            Assert.IsTrue(connected, "Failed to connect to SignalR hub.");
+            jihadkhawaja.mobilechat.client.MobileChat.Initialize(TestConfig.HubConnectionUrl);
+            await jihadkhawaja.mobilechat.client.MobileChat.SignalR.Connect(cancellationTokenSource);
         }
-
         [TestMethod]
-        public async Task SignIn()
+        public async Task ConnectTest()
         {
+            if (jihadkhawaja.mobilechat.client.MobileChat.SignalR.HubConnection.State == Microsoft.AspNetCore.SignalR.Client.HubConnectionState.Connected)
+            {
+                Assert.IsTrue(true);
+            }
+            else
+            {
+                Assert.IsTrue(false, "Failed to connect to SignalR hub.");
+            }
         }
 
         [TestMethod]
-        public async Task SignUp()
+        public async Task SignInTest()
+        {
+            dynamic? dynamicObj = await ChatAuthService.SignIn("test", "HvrnS4Q4zJ$xaW!3");
+            Dictionary<string, object>? result = null;
+            if (dynamicObj is not null)
+            {
+                result = JsonSerializer.Deserialize<Dictionary<string, object>>(dynamicObj);
+            }
+
+            Assert.IsNotNull(result, "Failed to sign in or user doesn't exist.");
+        }
+
+        [TestMethod]
+        public async Task SignUpTest()
+        {
+            dynamic? dynamicObj = await ChatAuthService.SignIn("test", "HvrnS4Q4zJ$xaW!3");
+            Dictionary<string, object>? result = null;
+            if (dynamicObj is not null)
+            {
+                result = JsonSerializer.Deserialize<Dictionary<string, object>>(dynamicObj);
+            }
+
+            if(result is not null)
+            {
+                Assert.IsNotNull(result, "Failed to sign in when user exist.");
+                return;
+            }
+
+            dynamic? dynamicObj2 = await ChatAuthService.SignUp("Test", "test", "test@domain.com", "HvrnS4Q4zJ$xaW!3");
+            Dictionary<string, object>? result2 = null;
+            if (dynamicObj2 is not null)
+            {
+                result2 = JsonSerializer.Deserialize<Dictionary<string, object>>(dynamicObj2);
+            }
+
+            Assert.IsNotNull(result2, "Failed to sign up or user already exist.");
+        }
+
+        [TestCleanup]
+        public async Task Cleanup()
         {
         }
     }
