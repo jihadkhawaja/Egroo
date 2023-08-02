e6c07897474cf0175d3db4fca30c7ef213eb0a15
Jihad Khawaja <khawajajihad@gmail.com>
2023-08-02 12:32:34 +0300

    Added Tests + Cleanup

---
+++ src/MobileChat.Server.Test/MessageTest.cs
@@ -0,0 +1,84 @@
+﻿using jihadkhawaja.mobilechat.client.Interfaces;
+using jihadkhawaja.mobilechat.client.Models;
+using jihadkhawaja.mobilechat.client.Services;
+using System.Text.Json;
+
+namespace MobileChat.Server.Test
+{
+    [TestClass]
+    public class MessageTest
+    {
+        private IChatAuth ChatAuthService { get; set; } = null!;
+        private IChatChannel ChatChannelService { get; set; } = null!;
+        private IChatMessage ChatMessageService { get; set; } = null!;
+
+        private static Channel Channel { get; set; }
+        private static User User { get; set; }
+
+        [TestInitialize]
+        public async Task Initialize()
+        {
+            ChatAuthService = new ChatAuthService();
+            ChatChannelService = new ChatChannelService();
+            ChatMessageService = new ChatMessageService();
+
+            dynamic? dynamicObj = await ChatAuthService.SignIn("test", "HvrnS4Q4zJ$xaW!3");
+            Dictionary<string, object>? result = null;
+            if (dynamicObj is not null)
+            {
+                result = JsonSerializer.Deserialize<Dictionary<string, object>>(dynamicObj);
+            }
+
+            //check user
+            if (result is not null)
+            {
+                Guid Id = Guid.Parse(result["id"].ToString());
+                string Token = result["token"].ToString();
+
+                User = new()
+                {
+                    Id = Id,
+                    Token = Token
+                };
+
+                var cancellationTokenSource = new CancellationTokenSource();
+                jihadkhawaja.mobilechat.client.MobileChat.Initialize(TestConfig.HubConnectionUrl, User.Token);
+                await jihadkhawaja.mobilechat.client.MobileChat.SignalR.Connect(cancellationTokenSource);
+
+                Channel = await ChatChannelService.CreateChannel("test");
+            }
+            else
+            {
+                var cancellationTokenSource = new CancellationTokenSource();
+                jihadkhawaja.mobilechat.client.MobileChat.Initialize(TestConfig.HubConnectionUrl);
+                await jihadkhawaja.mobilechat.client.MobileChat.SignalR.Connect(cancellationTokenSource);
+            }
+        }
+        [TestMethod]
+        public async Task SendMessageTest()
+        {
+            Message message = new()
+            {
+                ChannelId = Channel.Id,
+                Content = "This is a test.",
+                DisplayName = User.DisplayName,
+                SenderId = User.Id,
+            };
+
+            Assert.IsTrue(await ChatMessageService.SendMessage(message), "Failed to send message.");
+        }
+
+        [TestMethod]
+        public async Task ReadMessagesTest()
+        {
+            Message[] messages = await ChatMessageService.ReceiveMessageHistory(Channel.Id);
+
+            Assert.IsNotNull(messages, "Failed to read messages.");
+        }
+
+        [TestCleanup]
+        public async Task Cleanup()
+        {
+        }
+    }
+}
---
+++ src/MobileChat.Server.Test/TestConfig.cs
@@ -0,0 +1,7 @@
+﻿namespace MobileChat.Server.Test
+{
+    public static class TestConfig
+    {
+        public static string HubConnectionUrl = "http://localhost:5175/chathub";
+    }
+}
