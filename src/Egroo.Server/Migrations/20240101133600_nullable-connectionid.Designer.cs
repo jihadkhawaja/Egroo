c2b6031b6fe7067375e60941f6956dae1dfb8288
Jihad Khawaja <khawajajihad@gmail.com>
2024-01-01 15:39:32 +0200

    bug fix

--- src/Egroo.Server/Migrations/20240101133600_nullable-connectionid.Designer.cs
+++ src/Egroo.Server/Migrations/20240101133600_nullable-connectionid.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using jihadkhawaja.mobilechat.server.Database;
@@ -11,9 +12,11 @@ using jihadkhawaja.mobilechat.server.Database;
 namespace Egroo.Server.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20240101133600_nullable-connectionid")]
+    partial class nullableconnectionid
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
