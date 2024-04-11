d0cff938f4c54c8d0cbb3b33c1d6b03dda6f20f3
Jihad Khawaja <khawajajihad@gmail.com>
2024-04-11 23:54:15 +0300

    Improved components integrity

--- src/Egroo.UI/Services/SessionStorage.cs
+++ src/Egroo.UI/Services/SessionStorage.cs
@@ -6,5 +6,15 @@ namespace Egroo.UI.Services
     {
         public string? Token { get; set; }
         public User? User { get; set; }
+        public AppState AppState { get; set; }
+    }
+
+    public enum AppState
+    {
+        INITIATING = 0,
+        LOADING_CACHE = 1,
+        ESTABLISHING_CONNECTION = 2,
+        CREATING_SESSION = 3,
+        CONNECTED = 4,
     }
 }
